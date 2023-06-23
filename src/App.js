757e0e95bfc502a1f765f092ed2b598b1c2c9678
Usama Ameen <Drop X Out>
2023-06-24 01:34:33 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Shop from './components/Shop'
 import './App.css';
+import Cart from './components/Cart';
 
-function App() {
+const App = () => {
+
+  const [cart,setCart] = useState([]);
+  const [warning,setWarning] = useState(false);
+  const[show,setShow] = useState(true);
+
+  const handleClick=(item) =>{
+    let isPresent = false;
+    cart.forEach((product) =>{
+      if(item.id === product.id)
+      isPresent= true;
+    })
+
+    if(isPresent){
+      setWarning(true);
+      setTimeout(()=>{
+        setWarning(false);
+      },2000);
+      return;
+    }
+    setCart([...cart,item]);
+  }
+
+  const handleChange = (item,d) => {
+    let ind = -1;
+    cart.forEach((data,index) => {
+      if(data.id === item.id)
+      ind = index;
+    });
+    const tempArr = cart;
+    tempArr[ind].amount +=d;
+    console.log(tempArr);
+
+    if(tempArr[ind].amount === 0){
+      tempArr[ind].amount =1;
+      
+    }
+    setCart([...tempArr])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar size={cart.length} setShow={setShow}/>
+      {
+        show ? <Shop handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange}/>
+      }
+      
+      {
+        warning && <div className='warning'> Item is already in your cart </div>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
