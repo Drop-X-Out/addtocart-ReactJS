5f3bee0b894114045702fd4caa234478915132a9
SHWETA-KALE <it.a.47.shweta.kale@mail.com>
2023-10-09 23:24:58 +0530

    contribution of login page

--- src/App.js
+++ src/App.js
@@ -1,61 +1,27 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import Shop from './components/Shop'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Otp from './pages/Otp';
+import Error from './pages/Error';
+import Headers from './components/Headers';
+import { Routes, Route } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Cart from './components/Cart';
 
-const App = () => {
-
-  const [cart,setCart] = useState([]);
-  const [warning,setWarning] = useState(false);
-  const[show,setShow] = useState(true);
-
-  const handleClick=(item) =>{
-    let isPresent = false;
-    cart.forEach((product) =>{
-      if(item.id === product.id)
-      isPresent= true;
-    })
-
-    if(isPresent){
-      setWarning(true);
-      setTimeout(()=>{
-        setWarning(false);
-      },2000);
-      return;
-    }
-    setCart([...cart,item]);
-  }
-
-  const handleChange = (item,d) => {
-    let ind = -1;
-    cart.forEach((data,index) => {
-      if(data.id === item.id)
-      ind = index;
-    });
-    const tempArr = cart;
-    tempArr[ind].amount +=d;
-    console.log(tempArr);
-
-    if(tempArr[ind].amount === 0){
-      tempArr[ind].amount =1;
-      
-    }
-    setCart([...tempArr])
-  }
-
+function App() {
   return (
-    <div>
-      <Navbar size={cart.length} setShow={setShow}/>
-      {
-        show ? <Shop handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange}/>
-      }
-      
-      {
-        warning && <div className='warning'> Item is already in your cart </div>
-      }
-    </div>
-  )
+    <>
+      <Headers />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/user/otp' element={<Otp />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
